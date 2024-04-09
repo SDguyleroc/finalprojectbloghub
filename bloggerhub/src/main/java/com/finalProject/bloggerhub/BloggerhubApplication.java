680ba8ce8281800afa624700493167a7b7b1f4e9
Guy-leroc <thonerguyleroc1@gmail.com>
2024-04-09 10:48:34 -0400

    intial commmit

---
+++ bloggerhub/src/main/java/com/finalProject/bloggerhub/BloggerhubApplication.java
@@ -0,0 +1,13 @@
+package com.finalProject.bloggerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggerhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggerhubApplication.class, args);
+	}
+
+}
---
+++ bloggerhub/src/test/java/com/finalProject/bloggerhub/BloggerhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalProject.bloggerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
