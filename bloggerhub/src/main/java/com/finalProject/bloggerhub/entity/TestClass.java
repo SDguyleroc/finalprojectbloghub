e4f554545e1bda5e90145c8066d0529e68fd5105
Guy-leroc <thonerguyleroc1@gmail.com>
2024-04-09 12:26:57 -0400

    added test class to test that my sql db is working.

---
+++ bloggerhub/src/main/java/com/finalProject/bloggerhub/entity/TestClass.java
@@ -0,0 +1,10 @@
+package com.finalProject.bloggerhub.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class TestClass {
+    @Id
+    private Long id;
+}
