7f1b44b5ddce0ef95e79463c10809437e9c75d04
Guy-leroc Ossebi <126127721+SDguyleroc@users.noreply.github.com>
2024-04-15 14:31:11 -0400

    Update CommentController.java
    javacdoc

--- bloggerhub/src/main/java/com/finalProject/bloggerhub/controller/CommentController.java
+++ bloggerhub/src/main/java/com/finalProject/bloggerhub/controller/CommentController.java
@@ -6,6 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * @author Guy-leroc Ossebi
+ * Controller class for managing blog post comments.
+ */
+
 @RestController
 @RequestMapping("/api/")
 @CrossOrigin(origins = "*")
